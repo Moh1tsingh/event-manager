8e8576463431ea657fa1dcf1f3f1f2bfed98be33
Moh1tsingh <thakurmohitsingh2003@gmail.com>
2024-07-08 13:45:52 +0530

    fix

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 import React from 'react'
-import Confetti from "react-confetti";
+// import Confetti from "react-confetti";
 function Success() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      {window ? <Confetti width={window.innerWidth} height={window.innerHeight} numberOfPieces={300} recycle={false} className="w-full h-full" /> : null}
+      {/* {window ? <Confetti width={window.innerWidth} height={window.innerHeight} numberOfPieces={300} recycle={false} className="w-full h-full" /> : null} */}
       
       <h1 className=" text-4xl text-green-500 font-semibold">
         You're RSVP was successfull!
