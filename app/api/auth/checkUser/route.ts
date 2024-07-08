4d58b87805d8a2f240a36a03b1a644e2849a6ead
Moh1tsingh <thakurmohitsingh2003@gmail.com>
2024-07-08 13:54:11 +0530

    fix post login

--- app/api/auth/checkUser/route.ts
+++ app/api/auth/checkUser/route.ts
@@ -31,6 +31,6 @@ export async function GET(){
     return NextResponse.redirect(
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000/dashboard"
-        : "https://reddit-clone-deployment.vercel.app/"
+        : "https://event-manager-liard-one.vercel.app/"
     );
 }   
