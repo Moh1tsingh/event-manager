be0033cfc739890832bcf335d1d382982662f2e4
Moh1tsingh <thakurmohitsingh2003@gmail.com>
2024-07-08 14:05:12 +0530

    feat add toast

--- app/components/CopyLinkButton.tsx
+++ app/components/CopyLinkButton.tsx
@@ -1,18 +1,23 @@
-"use client"
-import { Button } from '@/components/ui/button'
-import { Copy } from 'lucide-react';
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
+import React from "react";
+import { useToast } from "@/components/ui/use-toast";
 
-function CopyLinkButton({id}: {id: string}) {
+function CopyLinkButton({ id }: { id: string }) {
+  const { toast } = useToast();
   return (
     <div>
       <Button
         variant={"default"}
-        onClick={() =>
+        onClick={() => {
           navigator.clipboard.writeText(
             `https://event-manager-liard-one.vercel.app/${id}`
-          )
-        }
+          );
+          toast({
+            title: "Link Copied to Clipboard!",
+          });
+        }}
         className="w-10 p-0 hover:bg-gray-100 hover:text-gray-800 duration-200 ease-linear"
       >
         <Copy className="size-6" />
@@ -21,4 +26,4 @@ function CopyLinkButton({id}: {id: string}) {
   );
 }
 
-export default CopyLinkButton
+export default CopyLinkButton;
